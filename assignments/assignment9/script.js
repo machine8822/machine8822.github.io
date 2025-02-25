e3d8341cfd00b105e795a8a752a9270dc90152ca
King <willytk249@gmail.com>
2025-02-24 19:01:06 -0500

    assignment9 complete

--- assignments/assignment9/script.js
+++ assignments/assignment9/script.js
@@ -1,32 +1,85 @@
-//Display stairs and the Climb stairs button
-function drawStairs() {
-    alert("Drawing stairs beg");
+//Create the stairs itself
+function makeStairs() {
+    console.log("Creating stairs");
 
-    let temp = "";
-    const staDisplay = document.getElementById("main-content");
-    let stairDis = document.getElementById("txt-stairs");
+    let stairText = document.getElementById("whole-stairs");
+    stairText.innerHTML = "";
+    let stairSeg = "|____|";
 
-    staDisplay.classList.toggle("hidden");
+    //Create each stair segment
+    for(let i=0; i<10; i++) {
+     /*   stairText.innerHTML += i;
+        stairSeg = stairSeg + "\n";
+        stairText.innerHTML = stairSeg; */
 
-    for(let i=0; i<stairArray.length; i++) {
-        stairArray[i] = "ab";
+        const liElem = document.createElement("li");
+        liElem.style.zIndex = 1;
+        
+        liElem.textContent = stairSeg;
+
+        stairText.append(liElem);
+
+        console.log(stairText.innerHTML);
     }
 
-    for(let i=0; i<stairArray.length; i++) {
-        temp = temp + "\n" + stairArray[i];
-        console.log(temp);
-        alert("logging");
-    }
-    stairDis = temp;
-
-
-    alert("Done Drawing stairs");
+    //console.log(stairSeg);
+    console.log("Done creating stairs");
+    disStairs();
 }
 
+//Display stairs
+function disStairs() {
+    console.log("Displaying stairs");
+    const staDis = document.getElementById("main-content");
 
-  
-let stairArray = [];
+    staDis.classList.toggle("hidden");
+}
+
+//Start the time
+function climbStairs() {
+    console.log("Starting climb");
+    timer = setInterval(climbing, 1500);
+}
+//Make the image move each interval
+function climbing() {
+    console.log("Moving");
+    var imgMov = document.getElementById("climb-test");
+    temp++;
+    console.log(temp);
+
+    if(temp === 1) {
+        imgMov.src = "images/hisRight.png";
+        imgMov.style.top = '-280px';
+        
+    }
+    if(temp === 2) {
+        imgMov.src = "images/hisLeft.png";
+        imgMov.style.top = '-360px';
+        
+    }
+    if(temp === 3) {
+        imgMov.src = "images/hisRight.png";
+        imgMov.style.top = '-450px';
+        
+    }
+    if(temp === 4) {
+        imgMov.src = "images/hisLeft.png";
+        imgMov.style.top = '-540px';
+        
+    }
+    if(temp === 5) {
+        clearInterval(timer);
+        console.log("Done moving");
+    }
+}
+//Stop the time
+function stopClimb() {
+    clearInterval(timer);
+}
+let timer;
+let temp = 0;
 window.onload = () => {
-    document.getElementById("stair").onpointerdown = drawStairs;
-
+   // document.getElementById("stair").onpointerdown = disStairs;
+    document.getElementById("stair").onpointerdown = makeStairs;
+    document.getElementById("climb").onpointerdown = climbStairs;
 }
