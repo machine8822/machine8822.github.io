a466c0d7f8fc3990d71c0eee37606478340bfc77
King <willytk249@gmail.com>
2025-02-27 14:18:24 -0500

    assignment11 draft

---
+++ assignments/assignment11/script.js
@@ -0,0 +1,20 @@
+//6 different attributes
+class Pizza {
+    constructor(name, ingredients, sauce, cheese, price, pic) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.price = price;
+        this.pic = pic;
+    }
+}
+
+
+window.onload = () => {
+    let pizzas = [];
+
+
+
+    
+}
